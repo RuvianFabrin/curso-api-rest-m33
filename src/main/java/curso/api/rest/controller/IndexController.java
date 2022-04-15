a4f5e8b6283bcb3e564440a3894fb221e256854f
Ruvian <ruvian.fabrin@hotmail.com>
2022-04-14 22:38:23 -0300

    Criando serviço restful de consulta de usuario

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -2,37 +2,37 @@ package curso.api.rest.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import curso.api.rest.model.Usuario;
+import curso.api.rest.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
 
-    @GetMapping(value = "/",produces = "application/json")
-    public ResponseEntity <Usuario> init() {
-    	Usuario usuario = new Usuario();
-    	usuario.setId(50L);
-    	usuario.setLogin("teste@mail.com");
-    	usuario.setNome("Teste Nome");
-    	usuario.setSenha("abc123");
+	@Autowired /* Se Fosse CDI seria @Inject */
+	private UsuarioRepository usuarioRepository;
+
+    @GetMapping(value = "/{id}",produces = "application/json")
+    public ResponseEntity <Usuario> init(@PathVariable(value = "id") Long id) {		
     	
-    	Usuario usuario2 = new Usuario();
-    	usuario2.setId(20L);
-    	usuario2.setLogin("teste2@mail.com");
-    	usuario2.setNome("Teste Nome2");
-    	usuario2.setSenha("abc1232");
+		Usuario usuario = usuarioRepository.findById(id).get();
 
-		List<Usuario> usuarios = new ArrayList<Usuario>();
-		usuarios.add(usuario);
-		usuarios.add(usuario2);
-
-        return new ResponseEntity(usuarios,HttpStatus.OK);
+        return new ResponseEntity<Usuario>(usuario,HttpStatus.OK);
     }
+
+	@GetMapping(value = "/", produces = "application/json")
+	public ResponseEntity<List<Usuario>> usuario() {
+		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
+		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
+	}
 }
