bda9b2148b62be8ee6c2ffc0708e38fcd30663cd
Ruvian <ruvian.fabrin@hotmail.com>
2022-04-14 23:23:19 -0300

    Criando a atualização de cadastro com o PUT

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,7 @@ public class IndexController {
 
 	//Costumizar URL - @GetMapping(value = "/{id}/umNome/{venda}",produces = "application/json")
 	//init(@PathVariable(value = "id") Long id, @PathVariable(value = "venda") Long nome)
-    @GetMapping(value = "/{id}",produces = "application/json")
+    @GetMapping(value = "/{id}",produces = "application/json") //Pesquisa
     public ResponseEntity <Usuario> init(@PathVariable(value = "id") Long id) {		
     	
 		Usuario usuario = usuarioRepository.findById(id).get();
@@ -34,15 +35,23 @@ public class IndexController {
         return new ResponseEntity<Usuario>(usuario,HttpStatus.OK);
     }
 
-	@GetMapping(value = "/", produces = "application/json")
+	@GetMapping(value = "/", produces = "application/json")//Pesquisa
 	public ResponseEntity<List<Usuario>> usuario() {
 		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
 	}
 
-	@PostMapping(value = "/", produces = "application/json")
+	@PostMapping(value = "/", produces = "application/json") //Cadastra
 	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
 		Usuario usuarioSalvo = usuarioRepository.save(usuario);
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 	}
+
+	@PutMapping(value = "/", produces = "application/json") //Cadastra
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+	}
+
+
 }
